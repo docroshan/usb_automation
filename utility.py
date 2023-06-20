c7791ddf1d43ca55f1fba5845fd86a6d459ee57a
docroshan <135826049+docroshan@users.noreply.github.com>
2023-06-20 12:39:28 +0530

    Updated utility.py

--- utility.py
+++ utility.py
@@ -2,5 +2,6 @@ from config_parser import *
 
 user_config_file = get_attr('user_config.ini', 'user_config_file')
 
-print(f"{'-' * 15} Running test.py {'-' * 15}")
+print(f"{'-' * 25} Running utility.py {'-' * 25}")
 print(user_config_file)
+print(f"{'-' * 70}")
